7dbd7d75ef11c74f48ff33938095de7f6c15fb8a
ljyoth <ljyoth@users.noreply.github.com>
2024-11-26 16:15:25 -0500

    windows: support finding file names by process id

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 use std::env;
 
-use fileproc::processes;
+use fileproc::{files, processes};
 
 fn main() -> Result<(), Box<dyn core::error::Error>> {
-    let file_path = env::args().skip(1).next().ok_or("no file provided")?;
-    let res = processes(&file_path)?;
-    println!("{:?}", res);
+    // let file_path = env::args().skip(1).next().ok_or("no file provided")?;
+    // let res = processes(&file_path)?;
+    // println!("{:?}", res);
+    let res = files(23972)?;
+    res.iter().for_each(|p| {
+        println!("{}", p.to_string_lossy())
+    });
     Ok(())
 }
--- src/windows.rs
+++ src/windows.rs
@@ -5,18 +5,27 @@ use std::{
 
 use windows::{
     Wdk::{
-        Storage::FileSystem::{FileProcessIdsUsingFileInformation, NtQueryInformationFile},
+        Foundation::{NtQueryObject, ObjectTypeInformation, OBJECT_INFORMATION_CLASS},
+        Storage::FileSystem::{
+            FileProcessIdsUsingFileInformation, NtDuplicateObject, NtQueryInformationFile,
+        },
         System::{
-            SystemInformation::NtQuerySystemInformation,
+            SystemInformation::{NtQuerySystemInformation, SYSTEM_INFORMATION_CLASS},
             SystemServices::FILE_PROCESS_IDS_USING_FILE_INFORMATION,
         },
     },
     Win32::{
-        Foundation::{GetLastError, HANDLE, HMODULE, MAX_PATH, STATUS_INFO_LENGTH_MISMATCH},
+        Foundation::{
+            GetLastError, HANDLE, HMODULE, MAX_PATH, STATUS_INFO_LENGTH_MISMATCH, UNICODE_STRING,
+        },
+        Storage::FileSystem::{GetFinalPathNameByHandleW, FILE_NAME_NORMALIZED},
         System::{
             ProcessStatus::GetModuleFileNameExW,
-            Threading::{OpenProcess, PROCESS_QUERY_INFORMATION, PROCESS_VM_READ},
-            WindowsProgramming::SYSTEM_PROCESS_INFORMATION,
+            Threading::{
+                GetCurrentProcess, OpenProcess, PROCESS_DUP_HANDLE, PROCESS_QUERY_INFORMATION,
+                PROCESS_VM_READ,
+            },
+            WindowsProgramming::PUBLIC_OBJECT_TYPE_INFORMATION,
             IO::IO_STATUS_BLOCK,
         },
     },
@@ -60,7 +69,6 @@ pub fn processes<P: AsRef<Path>>(file: P) -> Result<Vec<Process>, Error> {
     let process_ids = unsafe {
         loop {
             let mut io_status_block = IO_STATUS_BLOCK::default();
-            // See https://stackoverflow.com/questions/47507578/winapi-get-the-process-which-has-specific-handle-of-a-file/47510579#47510579
             if let Err(err) = NtQueryInformationFile(
                 HANDLE(file.as_raw_handle()),
                 &mut io_status_block,
@@ -109,14 +117,158 @@ pub fn processes<P: AsRef<Path>>(file: P) -> Result<Vec<Process>, Error> {
     Ok(processes)
 }
 
-// pub fn files() -> Result<Vec<PathBuf>, Error> {
-//     unsafe { NtQuerySystemInformation(
-//         filehandle,
-//         iostatusblock,
-//         fileinformation,
-//         length,
-//         SystemExtendedInformation,
-//     ) };
-//     SYSTEM_PROCESS_INFORMATION;
-//     SYSTEM_HANDLE_INFORMATION
-// }
+#[repr(C)]
+#[derive(Debug)]
+/// from https://www.geoffchappell.com/studies/windows/km/ntoskrnl/api/ex/sysinfo/handle.htm
+struct SYSTEM_HANDLE_INFORMATION {
+    NumberOfHandles: usize,
+    Handles: [SYSTEM_HANDLE_TABLE_ENTRY_INFO_EX; 1],
+}
+
+#[repr(C)]
+#[derive(Debug)]
+// from https://www.geoffchappell.com/studies/windows/km/ntoskrnl/api/ex/sysinfo/handle_table_entry.htm?ts=0,97
+struct SYSTEM_HANDLE_TABLE_ENTRY_INFO {
+    UniqueProcessId: usize,
+    CreatorBackTraceIndex: u16,
+    ObjectTypeIndex: u16,
+    HandleAttributes: u32,
+    HandleValue: usize,
+    Object: *mut std::ffi::c_void,
+    GrantedAccess: u32,
+}
+
+#[repr(C)]
+#[derive(Debug)]
+/// from https://www.geoffchappell.com/studies/windows/km/ntoskrnl/api/ex/sysinfo/handle_ex.htm
+struct SYSTEM_HANDLE_INFORMATION_EX {
+    NumberOfHandles: usize,
+    Reserved: usize,
+    Handles: [SYSTEM_HANDLE_TABLE_ENTRY_INFO_EX; 1],
+}
+
+#[repr(C)]
+#[derive(Debug)]
+// from https://www.geoffchappell.com/studies/windows/km/ntoskrnl/api/ex/sysinfo/handle_table_entry_ex.htm?ts=0,97
+struct SYSTEM_HANDLE_TABLE_ENTRY_INFO_EX {
+    Object: *mut std::ffi::c_void,
+    UniqueProcessId: usize,
+    HandleValue: usize,
+    GrantedAccess: u32,
+    CreatorBackTraceIndex: u16,
+    ObjectTypeIndex: u16,
+    HandleAttributes: u32,
+    Reserved: u32,
+}
+
+pub fn files(pid: usize) -> Result<Vec<PathBuf>, Error> {
+    const SYSTEM_HANDLE_INFORMATION: SYSTEM_INFORMATION_CLASS = SYSTEM_INFORMATION_CLASS(0x10);
+    const SYSTEM_EXTENDED_HANDLE_INFORMATION: SYSTEM_INFORMATION_CLASS =
+        SYSTEM_INFORMATION_CLASS(0x40);
+    let mut buffer: Vec<u8> = Vec::with_capacity(size_of::<SYSTEM_HANDLE_INFORMATION_EX>());
+    let handle_info = unsafe {
+        loop {
+            let mut length = u32::default();
+            if let Err(err) = NtQuerySystemInformation(
+                SYSTEM_EXTENDED_HANDLE_INFORMATION,
+                buffer.as_mut_ptr() as _,
+                buffer.len() as _,
+                &mut length,
+            )
+            .ok()
+            {
+                if err.code() != STATUS_INFO_LENGTH_MISMATCH.to_hresult() {
+                    break Err(err);
+                }
+                buffer.resize(length as usize, 0);
+                dbg!(buffer.len());
+            } else {
+                let handle_info = buffer.as_ptr() as *const SYSTEM_HANDLE_INFORMATION_EX;
+                break Ok(&*handle_info);
+            }
+        }
+    }?;
+    println!("{:?}", handle_info);
+    let mut files = Vec::with_capacity(handle_info.NumberOfHandles);
+    unsafe {
+        let proc_handle = OpenProcess(PROCESS_DUP_HANDLE, false, pid as u32)?;
+
+        let mut length = u32::default();
+        let mut type_info_buf: Vec<u8> =
+            Vec::with_capacity(size_of::<PUBLIC_OBJECT_TYPE_INFORMATION>());
+        let mut path_buf: Vec<u16> = vec![0; MAX_PATH as usize];
+        let mut ptr = &handle_info.Handles as *const SYSTEM_HANDLE_TABLE_ENTRY_INFO_EX;
+        for _ in 0..handle_info.NumberOfHandles {
+            let handle_entry = &*ptr;
+            if handle_entry.UniqueProcessId == pid {
+                let mut handle = HANDLE::default();
+                if NtDuplicateObject(
+                    proc_handle,
+                    HANDLE(handle_entry.HandleValue as _),
+                    GetCurrentProcess(),
+                    Some(&mut handle),
+                    0,
+                    0,
+                    0,
+                )
+                .is_ok()
+                {
+                    let object_type_info = loop {
+                        if let Err(err) = NtQueryObject(
+                            HANDLE(handle_entry.HandleValue as _),
+                            ObjectTypeInformation,
+                            Some(type_info_buf.as_mut_ptr() as _),
+                            type_info_buf.len() as _,
+                            Some(&mut length),
+                        )
+                        .ok()
+                        {
+                            if err.code() != STATUS_INFO_LENGTH_MISMATCH.to_hresult() {
+                                break Err(err);
+                            }
+                            type_info_buf.resize(length as usize, 0);
+                            dbg!(type_info_buf.len());
+                        } else {
+                            let object_type_info =
+                                type_info_buf.as_ptr() as *const PUBLIC_OBJECT_TYPE_INFORMATION;
+                            break Ok(&*object_type_info);
+                        }
+                    };
+                    if let Ok(object_type_info) = object_type_info {
+                        let type_name =
+                            String::from_utf16_lossy(object_type_info.TypeName.Buffer.as_wide());
+                        if type_name == "File" {
+                            // let mut len = 0;
+                            // const ObjectNameInformation: OBJECT_INFORMATION_CLASS =
+                            //     OBJECT_INFORMATION_CLASS(1);
+                            // NtQueryObject(
+                            //     HANDLE(handle_entry.HandleValue as _),
+                            //     ObjectNameInformation,
+                            //     Some(buf.as_mut_ptr() as _),
+                            //     buf.capacity() as _,
+                            //     Some(&mut len),
+                            // )
+                            // .ok()?;
+                            // let name = buf.as_ptr() as *const UNICODE_STRING;
+                            // let name = String::from_utf16_lossy((*name).Buffer.as_wide());
+
+                            let len = GetFinalPathNameByHandleW(
+                                HANDLE(handle_entry.HandleValue as _),
+                                &mut path_buf,
+                                FILE_NAME_NORMALIZED,
+                            );
+                            // TODO: handle error
+                            if GetLastError().is_ok() {
+                                let name = String::from_utf16_lossy(&path_buf[0..len as usize]);
+                                let path = PathBuf::from(&name[r#"\\?\"#.len()..]);
+                                files.push(path);
+                            }
+                        }
+                    }
+                };
+            }
+            ptr = ptr.add(1);
+        }
+    };
+    Ok(files)
+}
